9f4aaf96d00261add983bb70278448fe23a5be20
Tao Tao <ttao@cs.unc.edu>
2021-11-25 20:03:12 -0500

    wire solver to main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main() {
 #if RUN_TEST
     test_all();
 #endif
-    auto cnf = parse_cnf();
-    print_cnf(*cnf);
+
+    auto formula = Formula(*parse_cnf());
+    cout << dpll(formula) << endl;
 
     return 0;
 }
