7990d88bdee6b699e940057988dbabee2e0c996d
Tao Tao <ttao@cs.unc.edu>
2021-11-27 13:03:57 -0500

    fix test file

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #unsat
 for file in unsat/*
 do
-  cmake-build-release/par_DPLL < "$file"
+  cmake-build-release/par_DPLL "$file"
 done
 
 # sat
 for file in sat/*
 do
-  cmake-build-release/par_DPLL < "$file"
+  cmake-build-release/par_DPLL "$file"
 done
